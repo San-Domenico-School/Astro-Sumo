d453e4039b3a4b8c1bc0210b8c7a0a539f05492e
Bruce Gustin <35908618+bgustin53@users.noreply.github.com>
2026-03-22 13:51:33 -0700

    Fix all powerups

--- Assets/Scripts/Configurations/PowerUpData.cs
+++ Assets/Scripts/Configurations/PowerUpData.cs
@@ -23,6 +23,7 @@ public class PowerUpData : ScriptableObject
         public string powerUpName;                // Name of powerup
         public Color colorIndicator;              // Color of light around player indicating powerup
         public float duration;                    // Duration of effect
+        public Vector3 scale;                     // Change in Player size
 
     [Header("Core Combat/Push Mechanics")]
         public float pulseRadius = 0.5f;          // Size of collider on player
--- Assets/Scripts/Powerup Effects/TeacherExampleEffectHandler.cs
+++ Assets/Scripts/Powerup Effects/TeacherExampleEffectHandler.cs
@@ -17,8 +17,6 @@
 * Jan 2, 2026
 **********************************************************************/
 
-using System;
-using System.Collections;
 using UnityEngine;
 public class TeacherExampleEffectHandler : MonoBehaviour
 {
@@ -27,37 +25,34 @@ private Vector3 originalScale;
     void OnEnable() 
     {
         // Subscribe to the global Power-Up events
-        PlayerPowerupHandler.OnPowerUpApplied += StretchPlayer;
-        PlayerPowerupHandler.OnPowerUpExpired += ResetScale;
+        PlayerPowerupHandler.OnPowerUpApplied += ApplyEffect;
+        PlayerPowerupHandler.OnPowerUpExpired += RemoveEffect;
     }
 
     void OnDisable() 
     {
         // Unsubscribe to prevent errors when this object is destroyed
-        PlayerPowerupHandler.OnPowerUpApplied -= StretchPlayer;
-        PlayerPowerupHandler.OnPowerUpExpired -= ResetScale;
+        PlayerPowerupHandler.OnPowerUpApplied -= ApplyEffect;
+        PlayerPowerupHandler.OnPowerUpExpired -= RemoveEffect;
     }
 
-    private void StretchPlayer(PowerUpData data) 
+    private void ApplyEffect(PowerUpData data) 
     {
         // We only trigger this if the power-up has a specific "fun" color
         // This prevents the player from stretching for EVERY power-up
-        if (data.powerUpName.Equals("Teacher Example"))
+        if (data.powerUpName.Equals("Noodle"))
         {
             originalScale = transform.parent.localScale;
             // Stretch the player vertically like a noodle
-            transform.parent.localScale = new Vector3(0.75f, .75f, 0.75f);
+            transform.parent.localScale = data.scale;
             Debug.Log("Power-Up Applied: I'm a noodle!");
         }
     }
 
-    private void ResetScale(PowerUpData data) 
+    private void RemoveEffect(PowerUpData data) 
     {
-        if (data.powerUpName.Equals("Teacher Example"))
-        {
-            // Return the player to their original size
-            transform.parent.localScale = originalScale;
-            Debug.Log("Power-Up Expired: Back to normal size.");
-        }
+        transform.parent.localScale = originalScale;
+        Debug.Log("Power-Up Expired: Back to normal size.");
+
     }
 }
