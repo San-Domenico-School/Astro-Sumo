d3edb617433aaf7b99310b85adc1472687e8f657
thedisage <thedisage@gmail.com>
2026-03-25 11:01:37 -0700

    Phase 2

---
+++ Assets/Scripts/Powerup Effects/ExampleEffectHandler_Sufian.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ExampleEffectHandler_Sufian : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
