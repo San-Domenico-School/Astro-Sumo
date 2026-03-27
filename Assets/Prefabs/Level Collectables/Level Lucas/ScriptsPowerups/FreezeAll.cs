859346770d057342e93bbba87def9460d8adade9
lucaalioto1 <nlucaalioto@gmail.com>
2026-03-27 10:35:55 -0700

    j

--- Assets/Prefabs/Level Collectables/Level Lucas/ScriptsPowerups/FreezeAll.cs
+++ Assets/Prefabs/Level Collectables/Level Lucas/ScriptsPowerups/FreezeAll.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class FreezePowerUp : MonoBehaviour 
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float freezeDuration = 3f;
+
+    private void OnTriggerEnter(Collider other) 
     {
-        
+        // Check if the object that touched the powerup has the PlayerMovement script
+        PlayerMovement snagger = other.GetComponent<PlayerMovement>();
+
+        if (snagger != null) 
+        {
+            ApplyGlobalFreeze(snagger);
+            Destroy(gameObject); 
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void ApplyGlobalFreeze(PlayerMovement theWinner) 
     {
+        // Fixed syntax for finding objects
+        PlayerMovement[] allPlayers = Object.FindObjectsByType<PlayerMovement>(FindObjectsSortMode.None);
+
+        foreach (PlayerMovement p in allPlayers) 
+        {
+            // If this player is NOT the one who picked it up, freeze them
+            if (p != theWinner) 
+            {
+               // p.Freeze(freezeDuration);
+            }
+        }
         
+        Debug.Log(theWinner.name + " froze everyone else!");
     }
 }
