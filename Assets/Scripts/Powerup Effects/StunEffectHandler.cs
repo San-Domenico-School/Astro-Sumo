719fe402ae445ffc026c2d06405e87f431945622
thedisage <thedisage@gmail.com>
2026-04-20 10:57:49 -0700

    added debug.log

--- Assets/Scripts/Powerup Effects/StunEffectHandler.cs
+++ Assets/Scripts/Powerup Effects/StunEffectHandler.cs
@@ -23,29 +23,31 @@ public class StunEffectHandler : MonoBehaviour
 {
         // Declare fields as needed    
         // Shown only as an example   
-    private Vector3 originalScale;        		
+    private float originalMoveMagnitude;   
+    private PlayerMovement playerMovement;     		
 
     // Needed if you need to grab additional components from the player
     // such as the rigidbody shown
     void Awake()
     {
-        originalScale = GetComponentInParent<Transform>().localScale;
+        playerMovement = GetComponentInParent<PlayerMovement>();
+        originalMoveMagnitude = playerMovement.moveMagnitude;
     }
 
     // Called when this object becomes enabled and active
     // We subscribe to the global power-up events here
     void OnEnable()
     {
-    PlayerPowerupHandler.OnPowerUpApplied += ApplyEffect;
-    PlayerPowerupHandler.OnPowerUpExpired += RemoveEffect;
+        PlayerPowerupHandler.OnPowerUpApplied += ApplyEffect;
+        PlayerPowerupHandler.OnPowerUpExpired += RemoveEffect;
     }
 
     // Called when this object is disabled or destroyed
     // We must unsubscribe to prevent errors and unwanted behavior
     void OnDisable()
     {
-    PlayerPowerupHandler.OnPowerUpApplied -= ApplyEffect;
-    PlayerPowerupHandler.OnPowerUpExpired -= RemoveEffect;
+        PlayerPowerupHandler.OnPowerUpApplied -= ApplyEffect;
+        PlayerPowerupHandler.OnPowerUpExpired -= RemoveEffect;
     }
 
     // Is called when the effect begins
@@ -53,20 +55,16 @@ public class StunEffectHandler : MonoBehaviour
     private void ApplyEffect(PowerUpData data) 
         {
             // This prevents the player from stretching for EVERY power-up
-            if (data.powerUpName.Equals("Noodle"))
+            if (data.powerUpName.Equals("Stun"))
             {
-                // Grabs the new scale from the PowerUpData file
-                // and applies it to the parent object
-                transform.parent.localScale = data.scale;
-                Debug.Log("Power-Up Applied: I'm a noodle!");
+                playerMovement.moveMagnitude = data.stun;
+                Debug.Log("Stun");
             }
         }
 
     // Is called when the effect ends
         private void RemoveEffect(PowerUpData data) 
         {
-            // Sets the scale back to where it was
-            transform.parent.localScale = originalScale;
-            Debug.Log("Power-Up Expired: Back to normal size.");
+           playerMovement.moveMagnitude = originalMoveMagnitude;
         }
 }
