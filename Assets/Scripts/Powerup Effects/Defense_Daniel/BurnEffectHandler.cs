6a84ce8a7738b673b5bd03f09243962a7eca08a2
dantheman20245 <dantheman20245@gmail.com>
2026-04-24 09:36:28 -0700

    yay
    yaya

--- Assets/Scripts/Powerup Effects/Defense_Daniel/BurnEffectHandler.cs
+++ Assets/Scripts/Powerup Effects/Defense_Daniel/BurnEffectHandler.cs
@@ -1,41 +1,59 @@
 using UnityEngine;
 
-public class BurnEffectHandler : MonoBehaviour
+public class ReverseAttack : MonoBehaviour
 {
-    private Vector3 originalScale;
+      // Declare fields as needed    
+      // Shown only as an example   
+private Vector3 originalScale;  
+private Rigidbody Rigidbody;   
+private float originalmass;         
 
-    void Awake()
-    {
-        originalScale = GetComponentInParent<Transform>().localScale;
-    }
+// Needed if you need to grab additional components from the player
+// such as the rigidbody shown
+void Awake()
+{
+    originalScale = GetComponentInParent<Transform>().localScale;
+    Rigidbody = GetComponentInParent<Rigidbody>();
+    originalmass=Rigidbody.mass;
+}
 
-    void OnEnable()
-    {
-        PlayerPowerupHandler.OnPowerUpApplied += ApplyEffect;
-        PlayerPowerupHandler.OnPowerUpExpired += RemoveEffect;
-    }
+// Called when this object becomes enabled and active
+// We subscribe to the global power-up events here
+void OnEnable()
+{
+PlayerPowerupHandler.OnPowerUpApplied += ApplyEffect;
+PlayerPowerupHandler.OnPowerUpExpired += RemoveEffect;
+}
 
-    void OnDisable()
-    {
-        PlayerPowerupHandler.OnPowerUpApplied -= ApplyEffect;
-        PlayerPowerupHandler.OnPowerUpExpired -= RemoveEffect;
-    }
+// Called when this object is disabled or destroyed
+// We must unsubscribe to prevent errors and unwanted behavior
+void OnDisable()
+{
+PlayerPowerupHandler.OnPowerUpApplied -= ApplyEffect;
+PlayerPowerupHandler.OnPowerUpExpired -= RemoveEffect;
+}
 
-    private void ApplyEffect(PowerUpData data)
-    {
-        // If the Burn power-up is applied, kill the player
-        if (data.powerUpName.Equals("Burn"))
-        {
-            Debug.Log("Player burned!");
+// Is called when the effect begins
+// The PowerUpData parameter contains all configuration values
+private void ApplyEffect(PowerUpData data) 
+      {
+          // This prevents the player from stretching for EVERY power-up
+          if (data.powerUpName.Equals("Burn"))
+          {
+              // Grabs the new scale from the PowerUpData file
+              // and applies it to the parent object
+              transform.parent.localScale = data.scale;
+              Debug.Log("Power-Up Applied: I'm a noodle!");
+              Rigidbody.mass = data.massIncrease;
+          }
+      }
 
-            // Destroy the parent object (player)
-            Destroy(transform.parent.gameObject);
-        }
-    }
-
-    private void RemoveEffect(PowerUpData data)
-    {
-        // Optional: only needed if player survives and you want reset logic
-        transform.parent.localScale = originalScale;
-    }
+// Is called when the effect ends
+      private void RemoveEffect(PowerUpData data) 
+      {
+          // Sets the scale back to where it was
+          transform.parent.localScale = originalScale;
+          Debug.Log("Power-Up Expired: Back to normal size.");
+          Rigidbody.mass = originalmass;
+      }
 }
