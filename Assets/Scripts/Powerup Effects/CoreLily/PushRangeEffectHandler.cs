37a94d67992119f77c49b59c10c980b89c9db00b
Bruce Gustin <35908618+bgustin53@users.noreply.github.com>
2026-04-25 13:26:34 -0700

    Powerup Complete

--- Assets/Scripts/Powerup Effects/CoreLily/PushRangeEffectHandler.cs
+++ Assets/Scripts/Powerup Effects/CoreLily/PushRangeEffectHandler.cs
@@ -2,84 +2,66 @@ using UnityEngine;
 
 public class PushRangeEffectHandler : MonoBehaviour
 {
-      // Declare fields as needed    
-      // Shown only as an example   
- 
-private Rigidbody rigidbody;   
-private float originalPushRange;   	
+    // Declare fields as needed    
+    // Shown only as an example     
 
-private float currentPushRange;
+    private SphereCollider[] sphereColliders;
+    private SphereCollider sphereCollider; 
+    private float originalColliderRadius;   	
 
-// Needed if you need to grab additional components from the player
-// such as the rigidbody shown
-void Awake()
-{
-	
-    rigidbody = GetComponentInParent<Rigidbody>();
-    currentPushRange = originalPushRange;
-}
 
-// Called when this object becomes enabled and active
-// We subscribe to the global power-up events here
-void OnEnable()
-{
-PlayerPowerupHandler.OnPowerUpApplied += ApplyEffect;
-PlayerPowerupHandler.OnPowerUpExpired += RemoveEffect;
-}
-
-// Called when this object is disabled or destroyed
-// We must unsubscribe to prevent errors and unwanted behavior
-void OnDisable()
-{
-PlayerPowerupHandler.OnPowerUpApplied -= ApplyEffect;
-PlayerPowerupHandler.OnPowerUpExpired -= RemoveEffect;
-}
-
-// Is called when the effect begins
-// The PowerUpData parameter contains all configuration values
-private void ApplyEffect(PowerUpData data) 
-      {
-          // This prevents the player from stretching for EVERY power-up
-          if (data.powerUpName.Equals("IncreasePushRange"))
-          {
-              SphereCollider[] sphereColliders = GetComponentsInParent<SphereCollider>();
-             foreach(SphereCollider sphereCollider in sphereColliders)
-            {
-                if (sphereCollider.isTrigger)
-                {
-                    sphereCollider.radius = data.pulseRadius;
-                }
-            }
-              
-            
-              
-              Debug.Log("Power-Up Applied: I'm a pulser radius!");
-              
-            
-          }
-      }
-
-// Is called when the effect ends
-      private void RemoveEffect(PowerUpData data)
-
+    // Needed if you need to grab additional components from the player
+    // such as the rigidbody shown
+    void Awake()
     {
-        if (data.powerUpName.Equals("IncreasePushRange"))
-          {
-              SphereCollider[] sphereColliders = GetComponentsInParent<SphereCollider>();
-             foreach(SphereCollider sphereCollider in sphereColliders)
+        sphereColliders = GetComponentsInParent<SphereCollider>();
+        foreach(SphereCollider collider in sphereColliders)
+        {
+            if (collider.isTrigger)
             {
-                if (sphereCollider.isTrigger)
-                {
-                    sphereCollider.radius = 0.5f;
-                }
+                sphereCollider = collider; 
             }
-              
-            
-              
-              Debug.Log("radius normal!");
-              
-            
-          }
+        }
+        originalColliderRadius = sphereCollider.radius;
+    }
+
+    // Called when this object becomes enabled and active
+    // We subscribe to the global power-up events here
+    void OnEnable()
+    {
+    PlayerPowerupHandler.OnPowerUpApplied += ApplyEffect;
+    PlayerPowerupHandler.OnPowerUpExpired += RemoveEffect;
+    }
+
+    // Called when this object is disabled or destroyed
+    // We must unsubscribe to prevent errors and unwanted behavior
+    void OnDisable()
+    {
+    PlayerPowerupHandler.OnPowerUpApplied -= ApplyEffect;
+    PlayerPowerupHandler.OnPowerUpExpired -= RemoveEffect;
+    }
+
+    // Is called when the effect begins
+    // The PowerUpData parameter contains all configuration values
+    private void ApplyEffect(PowerUpData data) 
+    {
+        // This prevents the player from stretching for EVERY power-up
+        if (data.powerUpName.Equals("Increase Pickup Range"))
+        {
+            sphereCollider.radius = data.pulseRadius;  
+            Debug.Log("Power-Up Applied: I can pickup at a distance");  
+        }
+    }
+
+    // Is called when the effect ends
+    private void RemoveEffect(PowerUpData data)
+    {
+        if (data.powerUpName.Equals("Increase Pickup Range"))
+        {
+            sphereCollider.radius = originalColliderRadius;
+        }
+
+        Debug.Log("radius normal!");
     }
 }
 
