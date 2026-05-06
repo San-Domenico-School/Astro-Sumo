f8353b85efcd1f3e8d5836b287774f0020fcfd4c
Bruce Gustin <35908618+bgustin53@users.noreply.github.com>
2026-05-06 10:07:43 -0700

    WhoWon

--- Assets/Scripts/UI Controllers/ZNijjar/UIController.cs
+++ Assets/Scripts/UI Controllers/ZNijjar/UIController.cs
@@ -91,8 +91,8 @@ namespace ZNijjar.UI
                         //Reset starting fields
                         player.transform.position = player.originalSpawnPosition;
                         player.GetComponent<Rigidbody>().useGravity = true; 
-                        player.moveMagnitude = 250;
-                        player.linearDamping = 0.5f;
+                        player.moveMagnitude = 750;
+                        player.linearDamping = 2.5f;
                         player.appliedForce = 0;
                         player.controlsReversed = false;
                         player.isFrozen = false; 
